81c4e853b2127fc04a5d5b158ccf7e8e62b35a94
Maxx <pridian.101@gmail.com>
2017-10-03 23:37:38 +0900

    login commit

---
+++ login.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Login</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+		    <div class="login-page">
+			  <div class="form">
+			  	<h3>Teacher Login</h3>
+			    <form class="login-form">
+			      <input type="text" placeholder="username"/>
+			      <input type="password" placeholder="password"/>
+			      <button>login</button>
+			      <p class="message">Not registered? <a href="#">Create a profile</a></p>
+			    </form>
+			  </div>
+			</div>    
+    </div> <!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
