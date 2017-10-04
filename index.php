95c82c24ab7a68531332b13cc4f2e9616638a6f1
Maxx <pridian.101@gmail.com>
2017-10-04 10:30:32 +0900

    initial dashboard template

--- index.php
+++ index.php
@@ -7,25 +7,62 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Login</title>
+    <title>Create Profile</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="css/signin.css" rel="stylesheet">
+    <link href="css/dashboard.css" rel="stylesheet">
   </head>
 
   <body>
 
-    <div class="container">
-        <h1>Hello World</h1>
+    <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2 col-sm-4 sidebar1">
+                <div class="logo">
+                    <img src="http://sbis.deped.gov.ph/seal.gif" class="img-fluid center-block" alt="Logo">
+                </div>
+                <br>
+                <div class="left-navigation">
+                    <ul class="list">
+                        <h5><strong>Data Sheet</strong></h5>
+                        <li>Students</li>
+                        <li>Archive</li>
+                    </ul>
+
+                    <br>
+
+                    <ul class="list">
+                        <h5><strong>Quarters</strong></h5>
+                        <li>Quarter 1</li>
+                        <li>Quarter 1</li>
+                        <li>Quarter 1</li>
+                        <li>Quarter 1</li>
+                        <li>Summary of Grades</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-10 col-sm-8 main-content">
+            <!--Main content code to be written here --> 
+            <h1>Miss Tapya</h1>
+            
+        </div>
+    </div>
+</body>
+        <!-- /.MultiStep Form -->
+
     </div> <!-- /container -->
 
-    <!-- Bootstrap core JavaScript
+        <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/create_profile.js"></script>
   </body>
 </html>
